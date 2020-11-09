3fdd8c9a4fac847a53190ba67b818f1313bef22f
Jae-Hoon Yang <didwogns789@yonsei.ac.kr>
2020-11-09 11:58:45 +0900

    Serialization: Creating a model to work with

--- django_rest_framework_tutorial/settings.py
+++ django_rest_framework_tutorial/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "pygments",
+    "snippets.apps.SnippetsConfig",
 ]
 
 MIDDLEWARE = [
