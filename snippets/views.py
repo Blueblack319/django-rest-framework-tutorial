c695d4b2b0d32d98b6afe666f11ebff33a384c6f
Jae-Hoon Yang <didwogns789@yonsei.ac.kr>
2020-11-10 11:14:44 +0900

    Authentication & Permissions: Associating Snippets with Users

--- snippets/views.py
+++ snippets/views.py
@@ -14,6 +14,13 @@ class SnippetList(generics.ListCreateAPIView):
 
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    """
+    The user isn't sent as part of the serialized representation, 
+    but is instead a property of the incoming request.
+    """
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
