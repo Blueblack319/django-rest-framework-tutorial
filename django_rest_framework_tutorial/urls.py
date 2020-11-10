3f4d4008cc7dafbf13c12d1f80c9dab89c9879d1
Jae-Hoon Yang <didwogns789@yonsei.ac.kr>
2020-11-10 14:02:59 +0900

    Relationships & Hyperlinked APIs: Hyperlinking our API

--- django_rest_framework_tutorial/urls.py
+++ django_rest_framework_tutorial/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
+from snippets import views
 
 urlpatterns = [
-    path("", include("snippets.urls")),
+    path("", views.api_root),
     path("api-auth/", include("rest_framework.urls")),
 ]
--- snippets/views.py
+++ snippets/views.py
@@ -4,6 +4,7 @@ from rest_framework import permissions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+from rest_framework import renderers
 
 from .models import Snippet
 from .serializers import SnippetSerializer, UserSerializer
@@ -43,6 +44,15 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 
 class UserList(generics.ListAPIView):
